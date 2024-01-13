c5f935e3c266cac37df9d6cbb190330e7ce6570c
Ahmed Agha <ahmed.agha@argmax.ai>
2024-01-13 18:34:19 +0000

    add setup.sh for cml

---
+++ clearml/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Running setup script."
+
+# activate env
+source /root/miniconda3/envs/safe-slac/bin/python
+
+# go to the repo directory
+cd $CLEARML_GIT_ROOT
+
+# install deps
+pip install -r requirements.txt
+# now we need to tell clearml to use the python from our poetry env
+# this is in the general case (we use the system python above, so we could
+# have just hardcoded this as well)
+export python_path="/root/miniconda3/envs/safe-slac/bin/python"
+cat > $CLEARML_CUSTOM_BUILD_OUTPUT << EOL
+{
+  "binary": "$python_path",
+  "entry_point": "$CLEARML_GIT_ROOT/$CLEARML_TASK_SCRIPT_ENTRY",
+  "working_dir": "$CLEARML_GIT_ROOT/$CLEARML_TASK_WORKING_DIR"
+}
+EOL
