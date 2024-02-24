4971f5e12d033edf1bd12728510299613ecc633f
AAgha66 <ahmed.agha@argmax.ai>
2024-02-24 05:29:49 +0100

    revert last commit

--- train_safe.py
+++ train_safe.py
@@ -23,8 +23,7 @@ def get_git_short_hash():
 
 def main(args):
     config = get_default_config()
-    local = args.get("local", False)
-    if not local:
+    if not args.local:
         task = clearml.Task.init()
         task_params = task.get_parameters_as_dict(cast=True)
         d = task_params["internal"]
